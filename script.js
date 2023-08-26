a7e76cf24cf0ac3d9b9c60636bf650a000975816
Chandan1035 <lchandanrao01@gmail.com>
2023-08-26 23:45:56 +0530

    first commit

--- script.js
+++ script.js
@@ -18,33 +18,24 @@ signupBtn.addEventListener("click", () => {
     return;
   }
 
-  // Generate a random access token
   const accessToken = Math.random().toString(36).substring(7);
 
-  // Store the user state in local storage
   localStorage.setItem("username", username);
   localStorage.setItem("password", password);
   localStorage.setItem("access-token", accessToken);
 
-  // Redirect to the profile page
   window.location.href = "/profile";
 });
 
 logoutBtn.addEventListener("click", () => {
-  // Clear the user state from local storage
   localStorage.clear();
-
-  // Redirect to the signup page
   window.location.href = "/signup";
 });
 
-// Check if the user is logged in
 if (localStorage.getItem("access-token")) {
-  // The user is logged in, so redirect to the profile page
   window.location.href = "/profile";
 }
 
-// Redirect to the signup page if the user tries to access the profile page without an access token
 window.addEventListener("popstate", () => {
   if (!localStorage.getItem("access-token")) {
     window.location.href = "/signup";
